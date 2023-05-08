6ec3c635b3b7c1ce2899dddf1bb909a996878172
nomadunchecked <roman.lanyak@gmail.com>
2023-05-08 15:39:06 -0700

    Netify v.2

--- components/Footer.js
+++ components/Footer.js
@@ -5,9 +5,7 @@ export default () => html`
   <footer>
     <div class="one"></div>
     <div class="two"><h2>Get in Touch</h2></div>
-    <div class="three">
-      <span>Email:</span><a href="mailto:info@iTest.com">info@iTest.com</a> <a href="tel:123.456.8910"> <br /><span>Phone: </span> 123.456.8910</a>
-    </div>
+    <div class="three"><span>Email:</span><a href="mailto:info@iTest.com">info@iTest.com</a></div>
     <div class="four"><h5>&copy; 2023 iTest All rights reserved.</h5></div>
     <div class="five">
       <h4>
@@ -15,6 +13,8 @@ export default () => html`
         San Diego, CA 92122
       </h4>
     </div>
-    <div class="six"></div>
+    <div class="six">
+      <a href="tel:123.456.8910"> <br /><span>Phone: </span> 123.456.8910</a>
+    </div>
   </footer>
 `;
