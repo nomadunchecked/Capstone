94414e0f6cf1411b6dc61787393d23464dac4619
nomadunchecked <roman.lanyak@gmail.com>
2023-05-08 06:42:36 -0700

    test for Netlify

--- components/Footer.js
+++ components/Footer.js
@@ -3,21 +3,18 @@ import html from "html-literal";
 
 export default () => html`
   <footer>
-    <h5>&copy; 2023 iTest</h5>
-    <section id="contact-follow">
-      <div class="pull-left">
-        <a href="mailto:nomadunchecked@protonmail.com">nomadunchecked@protonmail.com</a>
-        <a href="tel:123.456.8910"><span>Call us: </span> 123.456.8910</a>
-      </div>
-      <div class="center"><h3>Get in Touch</h3></div>
-      <div class="pull-right">
-        <span>Follow: </span>
-        <ul class="social-links">
-          <li>Facebook (link to follow)</li>
-          <li>Twitter (link to follow)</li>
-          <!-- <li><a target="_blank" href="https://www.linkedin.com" title="LinkedIn"><i</a></li> -->
-        </ul>
-      </div>
-    </section>
+    <div class="one"></div>
+    <div class="two"><h2>Get in Touch</h2></div>
+    <div class="three">
+      <span>Email:</span><a href="mailto:info@iTest.com">info@iTest.com</a> <a href="tel:123.456.8910"> <br /><span>Phone: </span> 123.456.8910</a>
+    </div>
+    <div class="four"><h5>&copy; 2023 iTest All rights reserved.</h5></div>
+    <div class="five">
+      <h4>
+        123 Tech Dr <br />
+        San Diego, CA 92122
+      </h4>
+    </div>
+    <div class="six"></div>
   </footer>
 `;
--- components/views/Services.js
+++ components/views/Services.js
@@ -1,92 +1,49 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable no-unused-vars */
 import html from "html-literal";
+// import logoPic from "/assets/img/iTest.Logo4.png";
 
 export default () => html`
-
   <section id="jumbotron">
     <h1>SERVICES</h1>
     <h2>Get your software tested here</h2>
   </section>
 
-  <section class="services">
-  <div class="title-subtile-holder">
-    <!-- <div class="section-subtitle dark_subtitle">OUR QA SERVICES</div> -->
-    <h2 class="section-title dark_title">WHAT WE DO</h2>
-    <!-- <div class="section-border red_border"></div> -->
-  </div>
-
-      <div class="icon-content-holder">
-        <div class="icon-holder"></div>
-        <div class="content-holder">
-          <p class="content-title-holder">Web Testing</p>
-          <p class="content-text-holder">iTest offers website testing services and will systematically test every aspect of your website's technology including applications, client-server systems, eCommerce platforms and more.</p>
-        </div>
-      </div>
-
-      <div class="icon-list-holder">
-        <div class="icon-holder"></div>
-        <div class="content-holder">
-          <p class="content-title-holder">Mobile Testing</p>
-          <p class="content-text-holder">iTest software testing and quality assurance can test the functionality and user experience of your mobile app or responsive website across every major operating system.</p>
-        </div>
-      </div>
-
-      <div class="icon-list-holder">
-        <div class="icon-holder"></div>
-        <div class="content-holder">
-          <p class="content-title-holder">Responsive Testing</p>
-          <p class="content-text-holder">Responsive testing involves how a website or web application looks and behaves on different devices, screen sizes, and resolutions. iTest engineers will ensure that your website or web application can be used effectively on various devices, including desktops, laptops, tablets, and smartphones.</p>
-        </div>
-      </div>
-
-      <div class="icon-list-holder">
-        <div class="icon-holder"></div>
-        <div class="content-holder">
-          <p class="content-title-holder">Performance Testing</p>
-          <p class="content-text-holder">iTest engineers use Performance Testing to analyze various success factors such as response times and potential errors. With these results in hand,bottlenecks, bugs, and mistakes can be identified, and decisions made on how to optimize applications.</p>
-        </div>
-      </div>
-
-      <div class="icon-list-holder">
-        <div class="icon-holder"></div>
-        <div class="content-holder">
-          <p class="content-title-holder">Test Automation</p>
-          <p class="content-text-holder">iTest software testing and quality assurance can help you configure and maintain an automated testing framework for your application.</p>
-        </div>
-      </div>
-
-      <div class="icon-list-holder">
-        <div class="icon-holder"></div>
-        <div class="content-holder">
-          <p class="content-title-holder">Localization Testing</p>
-          <p class="content-text-holder">We offer software QA services can  audit your website or mobile application to ensure that it complies with local language and cultural standards.</p>
-        </div>
-      </div>
-
-      
-
-
-
-        <!-- 
-     
-      <div>
-        <h2>Responsive Testing</h2>
-        <p>Responsive testing is...</p>
-      </div>
-      <div>
-        <h2>Performance Testing</h2>
-        <p>Performance testing is...</p>
-      </div>
-      <div>
-        <h2></h2>
-        <p>Test automantion is...</p>
-      </div>
-      <div>
-        <h2>Localization Testing</h2>
-        <p>Web testing is...</p>
-      </div> -->
-
-      <!-- <div>Contact Form (form or link)</div> -->
-    </div>
+  <section class="section-title_main_title">
+    <h1>WHAT WE DO</h1>
   </section>
+
+  <div id="services">
+    <div class="one">
+      
+      <p id="content-title-holder">Web Testing</p>
+      <p id="content-text-holder">
+        iTest offers website testing services and will systematically test every aspect of your website's technology including applications, client-server systems, eCommerce platforms and more.
+      </p>
+    </div>
+    <div class="two">
+      <p id="content-title-holder">Mobile Testing</p>
+      <p id="content-text-holder">
+        iTest software testing and quality assurance can test the functionality and user experience of your mobile app or responsive website across every major operating system.
+      </p>
+    </div>
+    <div class="three">
+      <p id="content-title-holder">Responsive Testing</p>
+      <p id="content-text-holder">
+        iTest engineers will ensure that your website or web application can be used effectively on various devices, including desktops, laptops, tablets, and smartphones.
+      </p>
+    </div>
+    <div id="four">
+      <p id="content-title-holder">Performance Testing</p>
+      <p id="content-text-holder">iTest engineers use Performance Testing to analyze various success factors such as response times, bottlenecks and potential errors. With these results in hand, decisions can be made on how to optimize applications.</p>
+    </div>
+    <div class="five">
+      <p id="content-title-holder">Test Automation</p>
+      <p id="content-text-holder">iTest software testing and quality assurance can help you configure and maintain an automated testing framework for your application.</p>
+    </div>
+    <div class="six">
+    <p id="content-title-holder">Localization Automation</p>
+      <p id="content-text-holder">We offer software QA services can  audit your website or mobile application to ensure that it complies with local language and cultural standards.</</p>
+    </div>
+  </div>
 `;
