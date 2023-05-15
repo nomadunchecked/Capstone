4fe11310177edac91ab1bd3490ab5c868032ff9d
nomadunchecked <roman.lanyak@gmail.com>
2023-05-15 16:43:21 -0700

    Middleware implementation

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 
 // Initialize the Express application
 const app = express();
+
 dotenv.config();
 
 const PORT = process.env.PORT || 4040; // we use || to provide a default value
@@ -18,6 +19,25 @@ const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Connection Error:"));
 db.once("open", console.log.bind(console, "Successfully opened connection to Mongo!"));
+
+// LOGGING MIDDLEWEAR
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+app.use(cors);
+app.use(express.json());
+app.use(logging);
 
 // Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
